5d41562fae728351f6dda04e9fab6e9192808b10
pradipbankar0097 <pradipbankar0097@gmail.com>
2021-04-27 18:46:13 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+requirements = [
+    r.strip() for r in open("requirements.txt") if r.strip() and not r.strip().startswith("#")
+]
+
+
 setuptools.setup(
     name="iterfast",
     version="0.0.2",
     author="Pradip Bankar",
     author_email="pradipbankar0097@gmail.com",
     description="A library for fast iterative operations.",
+    install_requires=requirements,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pradipbankar0097/iterfast",
