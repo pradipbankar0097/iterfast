657a68e973b17d103de49b3a0023ecad19ca51d8
pradipbankar0097 <pradipbankar0097@gmail.com>
2021-04-27 11:57:53 +0530

    version changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="iterfast",
-    version="0.0.1",
+    version="0.0.2",
     author="Pradip Bankar",
     author_email="pradipbankar0097@gmail.com",
     description="A library for fast iterative operations.",
