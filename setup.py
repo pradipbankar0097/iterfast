1ea3b94c22ab7475f3e90233fac6cfa1b4f8a52c
pradipbankar0097 <62244159+pradipbankar0097@users.noreply.github.com>
2021-05-17 09:35:40 +0530

    version changed

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setuptools.setup(
     name="iterfast",
-    version="0.0.3",
+    version="0.0.4",
     author="Pradip Bankar",
     author_email="pradipbankar0097@gmail.com",
     description="A library for fast iterative operations.",
